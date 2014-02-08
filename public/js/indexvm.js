ee6f053ea2f52dbb11fe44e0650815462ac93fca
unknown <bob.craker@hotmail.com>
2014-02-08 20:07:58 +1300

    minified content

--- public/js/indexvm.js
+++ public/js/indexvm.js
@@ -1,24 +1,16 @@
 var viewModel;
 function vm(){
-	this.gamingDescription = function () {
-		$('.description').hide();
-		$('#aboutGaming').show();
-		console.log('le gaming description');
-	}
-	this.browsingDescription = function () {
-		$('.description').hide();
-		$('#aboutBrowsing').show();
-		console.log('browsing description');
-	}
-	this.videoEdittingDescription = function () {
-		$('.description').hide();
-		$('#aboutVideoEditting').show();
-		console.log('video editting description');
-	}
-	
 	this.budget = ko.observable(1337);
 	
-	this.descriptions = [];
+	self.hardwares = ko.observableArray([
+		{type: 'cpu', name: '4670', price: '294.40'},
+		{type: 'graphicsCard', name: 'r9 290', price: '746.35'},
+		{type: 'motherboard', name: 'ASRock H81M-HDS', price: '86.25'},
+		{type: 'RAM', name: 'G.SKILL Areas 8GB (2x4GB) DDR3 1333MHz', price: '113.85'},
+		{type: 'HDD', name: 'WD Blue Edition 1TB', price: '90.85'},
+		{type: 'PSU', name: 'CORSAIR 750W HX-750v2', price: '253.00'},
+		{type: 'case', name: 'COOLER MASTER N200', price: '90.85'},
+	]);
 }
 
 function sendBudget() {
